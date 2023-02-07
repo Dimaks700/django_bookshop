9897f822b4969d06f9681f902a958dd902a74b48
addiearm <addiearm@student.21-school.ru>
2023-02-07 16:50:20 +0300

    scripts for running server

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,6 @@
-docker build -t bookshop:1 .    
-docker run -it --rm -p 80:80 -e Token=' =&@5$w5d3m4-pez$9xifo_v2t4*sy5joy)gyl_@qptj@tnz%a5' bookshop:1
+pip3 install virtualenv
+python3 -m virtualenv myenv
+source myenv/bin/activate
+pip install -r requirements.txt
+export Token=" =&@5$w5d3m4-pez$9xifo_v2t4*sy5joy)gyl_@qptj@tnz%a5"      
+python manage.py runserver 0.0.0.0:80
---
+++ runserver_docker.sh
@@ -0,0 +1,2 @@
+docker build -t bookshop:1 .    
+docker run -it --rm -p 80:80 -e Token=' =&@5$w5d3m4-pez$9xifo_v2t4*sy5joy)gyl_@qptj@tnz%a5' bookshop:1
